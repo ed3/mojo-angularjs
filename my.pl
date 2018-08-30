fc17ca7b54b186ede5d6489dc9d0f8cf88a0c8ae
Ed <ed3@users.noreply.github.com>
2018-08-30 19:49:17 +0300

    first commit

---
+++ my.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl;
+use Mojolicious::Lite;
+use Mojolicious::Static;
+use DBI;
+
+our $dbh = DBI->connect("dbi:SQLite:dbname=my.db3","","", { RaiseError => 1 });
+helper db => sub { $dbh };
+
+get '/' => sub {
+	my $self = shift;
+	$self->render('index');
+};
+
+get '/all' => sub {
+	my $self = shift;
+	my $sth = $dbh->prepare("select id, name from notes");
+	$sth->execute;
+	my $all = [];
+#	while (my $row = $sth->fetchrow_hashref){
+#		push @$all, $row;
+#	}
+	while(my ($id, $name) = $sth->fetchrow_array()) {
+		push @$all,{id=>$id,name=>$name};
+	}
+	$self->render('index',json=>{all=>$all});
+};
+
+get '/row/:name' => sub {
+	my $self = shift;
+	my $key = $self->stash('name');
+	my $sth = $dbh->prepare("select id, name from notes where name='" . $key ."'");
+	$sth->execute;
+	my ($id, $name) = $sth->fetchrow;
+	$self->render('index',json=>{id=>$id,name=>$name});
+};
+
+app->start;
+__DATA__
+
+@@ layouts/default.html.ep
+<!doctype html>
+<html data-ng-app="my">
+<head>
+<title><%= title %></title>
+%= javascript '/mojo/jquery/jquery.js'
+%= javascript '/js/angular.min.js';
+%= javascript '/js/angular-route.min.js';
+%= javascript '/js/my.js';
+</head>
+<body>
+<div><%= content %></div>
+</body>
+</html>
+
+@@not_found.html.ep
+Not Found!
+
+@@ index.html.ep
+% layout 'default';
+% title 'my';
+<div data-ng-view></div>
---
+++ public/js/my.js
@@ -0,0 +1,18 @@
+angular.module('my', ['ngRoute']).config(function($routeProvider) {
+$routeProvider
+	.when('/', {templateUrl:'partials/index.html'})
+	.when('/all', {templateUrl:'partials/all.html',controller:'Ctrl1'})
+	.when('/row/:name', {templateUrl:'partials/one.html',controller:'Ctrl2'})
+	.otherwise({redirectTo: '/'});
+})
+.controller('Ctrl1', function($scope,$http) {
+	$http.get("/all").then(function(response){
+		$scope.all = response.data.all;
+	});
+})
+.controller('Ctrl2', function($scope,$routeParams,$http) {
+	$http.get("/row/"+$routeParams.name).then(function(response){
+		$scope.id = response.data.id;
+		$scope.name = response.data.name;
+	});
+});
